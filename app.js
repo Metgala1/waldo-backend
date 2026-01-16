9bd59c9eecde505aed0f9ce2d83402a8b166c9d3
ATAGWE ROGER <atagweroger2@gmail.com>
2026-01-16 11:23:52 +0100

    modified my cors for multile origins

--- app.js
+++ app.js
@@ -10,10 +10,24 @@ const app = express();
 app.use(helmet());
 
 // CORS setup
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://waldo-frontend-three.vercel.app"
+];
+
 app.use(cors({
-    origin: ["http://localhost:5173" , "https://waldo-frontend-three.vercel.app"], 
-    credentials: true,
+  origin: function(origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
+
 
 // Body parser
 app.use(express.json());
